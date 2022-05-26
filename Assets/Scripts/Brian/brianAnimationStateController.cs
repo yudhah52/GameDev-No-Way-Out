e1dca41d1f5b81d0ab699a876f74b74a8e18c601
Ridwan Raditya <ridwan.raditya01@gmail.com>
2022-05-26 16:46:37 +0700

    update brian

---
+++ Assets/Scripts/Brian/brianAnimationStateController.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class brianAnimationStateController : MonoBehaviour
+{
+    Animator animator;
+    int isWalkForwardHash,isRunningHash;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        isWalkForwardHash = Animator.StringToHash("isWalking");
+        isRunningHash = Animator.StringToHash("isRunning");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool isRunning = animator.GetBool(isRunningHash);
+        bool isWalkForward = animator.GetBool(isWalkForwardHash);
+        bool runPressed = Input.GetKey(KeyCode.RightShift);
+        bool forwardPressed = Input.GetKey(KeyCode.I);
+
+        //Walk Forward
+        if (!isWalkForward && forwardPressed)
+        {
+            animator.SetBool(isWalkForwardHash, true);
+        }
+
+        if (isWalkForward && !forwardPressed)
+        {
+            animator.SetBool(isWalkForwardHash, false);
+        }
+
+        //Run
+        if (!isRunning && (forwardPressed && runPressed))
+        {
+            animator.SetBool(isRunningHash, true);
+        }
+
+        if (isRunning && (!forwardPressed || !runPressed))
+        {
+            animator.SetBool(isRunningHash, false);
+        }
+    }
+}
